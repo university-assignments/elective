f695be34eee155b66c75d581a50f00adeff8077b
nvd05 <nvd05@bk.ru>
2024-03-05 21:21:46 +0500

    изменено место хранения файлов

--- scripts/loader.js
+++ scripts/loader.js
@@ -54,7 +54,7 @@ window.main = new class
 
 		if (this.options.file.length > 0)
 		{
-			this.users.importFile(this.options.file);
+			this.users.importFile('/storage/' + this.options.file + '.json');
 		}
 	}
 
