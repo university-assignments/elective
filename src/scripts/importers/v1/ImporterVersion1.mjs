89bc127b4b06030a8e175653f2efce4948e4d126
nvd05 <nvd05@bk.ru>
2024-06-10 23:35:00 +0500

    Добавлено чтение перевода фраз из файла

--- src/scripts/importers/v1/ImporterVersion1.mjs
+++ src/scripts/importers/v1/ImporterVersion1.mjs
@@ -1,5 +1,6 @@
 
 import { FileJSON } from '../../plugins/files/json/FileJSON.mjs';
+import { FileCSV } from '../../plugins/files/excel/FileCSV.mjs';
 import { Files } from '../../plugins/files/Files.mjs';
 
 import { ProfilesPhrases } from '../../database/profiles_phrases/ProfilesPhrases.mjs';
@@ -14,23 +15,38 @@ export class ImporterVersion1
 	 */
 	static async files (files)
 	{
-		const profiles_path = '../storage/users.json';
+		const storage_path = '../storage';
+
+		// ===== ===== ===== ===== =====
+
+		const profiles_path = storage_path + '/users.json';
 		const profiles_info = new FileJSON(profiles_path);
 		const profiles_file = await files.download(profiles_info);
 		const profiles_data = profiles_file.converted;
 
 		// ===== ===== ===== ===== =====
 
-		const sections_path = '../storage/tags.json';
+		const sections_path = storage_path + '/tags.json';
 		const sections_info = new FileJSON(sections_path);
 		const sections_file = await files.download(sections_info);
 		const sections_data = sections_file.converted;
 
 		// ===== ===== ===== ===== =====
 
+		const translated_path = storage_path + '/translated.csv';
+		const translated_info = new FileCSV(translated_path);
+
+		translated_info.separator_value = ',';
+
+		const translated_file = await files.download(translated_info);
+		const translated_data = translated_file.converted;
+
+		// ===== ===== ===== ===== =====
+
 		return {
 			profiles: profiles_data,
-			sections: sections_data
+			sections: sections_data,
+			translated: translated_data
 		};
 	}
 
@@ -42,9 +58,11 @@ export class ImporterVersion1
 	 */
 	static async initialize (profiles_phrases, profiles, phrases, files)
 	{
+		const downloader = await this.files(files);
+
+		const translated_map = new Map(downloader.translated);
 		const profiles_ids = new Map();
 		const phrases_ids = new Map();
-		const downloader = await this.files(files);
 
 		// ===== ===== ===== ===== =====
 		// profiles
@@ -71,11 +89,15 @@ export class ImporterVersion1
 					continue;
 				}
 
+				const russian = translated_map.has(english)
+					? translated_map.get(english)
+					: '';
+
 				const sections = typeof downloader.sections[english] === 'object'
 					? downloader.sections[english].join(', ')
 					: '';
 
-				phrases_ids.set(english, phrases.create(english, '', sections));
+				phrases_ids.set(english, phrases.create(english, russian, sections));
 			}
 		}
 	}
