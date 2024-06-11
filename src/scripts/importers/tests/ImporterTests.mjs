dba5aac0061261534664943a7809e647b44fd967
nvd05 <nvd05@bk.ru>
2024-06-11 22:12:21 +0500

    Update ImporterTests.mjs

--- src/scripts/importers/tests/ImporterTests.mjs
+++ src/scripts/importers/tests/ImporterTests.mjs
@@ -13,14 +13,16 @@ export class ImporterTests
 	 */
 	static initialize (profiles_phrases, profiles, phrases)
 	{
-		// [profiles] 10 => [1..11]
-		for (let profile_id = 1; profile_id <= 11; profile_id++)
+		// [profiles] 6 => [1..6]
+		// [used] 5 => [1..5]
+		for (let profile_id = 1; profile_id <= 6; profile_id++)
 		{
 			profiles.create('profile_' + profile_id);
 		}
 
-		// [phrases] 11 => [1..12]
-		for (let phrase_id = 1; phrase_id <= 12; phrase_id++)
+		// [phrases] 11 => [1..11]
+		// [used] 10 => [1..10]
+		for (let phrase_id = 1; phrase_id <= 11; phrase_id++)
 		{
 			phrases.create(
 				'english_' + phrase_id,
@@ -29,12 +31,14 @@ export class ImporterTests
 			);
 		}
 
-		// [profiles_phrases] profiles => phrases => 65 [1..66]
-		for (let profile_id = 1; profile_id <= 11; profile_id++)
+		// [profiles] 5 => [1..5]
+		// [phrases] 10 => [1..10]
+		// [profiles_phrases] 30 => [1..30]
+		for (let profile_id = 1; profile_id <= 5; profile_id++)
 		{
-			for (let phrase_id = 1; phrase_id <= profile_id; phrase_id++)
+			for (let phrase_id = 1; phrase_id <= profile_id * 2; phrase_id++)
 			{
-				profiles_phrases.create(profile_id, phrase_id, phrase_id % 2 == 1);
+				profiles_phrases.create(profile_id, phrase_id, phrase_id % 4 == 1);
 			}
 		}
 	}
